3807c715d57b2884b214b4a96c014448e35a1c75
shengxin <xiaofeixiaflying@163.com>
2019-03-07 13:52:12 +0800

    no message

--- FlutterNavite/Flutter/flutter_module/lib/main.dart
+++ FlutterNavite/Flutter/flutter_module/lib/main.dart
@@ -19,16 +19,20 @@ class App extends StatelessWidget{
 
 class Home extends StatelessWidget{
   Widget _listItemBuilder(BuildContext context, int index){
-    if(index.isOdd){
-      // 在每一列之前，添加一个1像素高的分隔线widget
-      return new Divider(
-        height: 1.0,
-        color: Color.fromRGBO(166, 165, 245, 1.0),
-      );
-    }
+//    if(index.isOdd){
+//      // 在每一列之前，添加一个1像素高的分隔线widget
+//      return new Divider(
+//        height: 1.0,
+//        color: Color.fromRGBO(166, 165, 245, 1.0),
+//      );
+//    }
 
-//    return Text(posts[index].title);
-      return Container(
+    return new GestureDetector(
+      onTap: () {
+        //处理点击事件
+        debugPrint('$index');
+      },
+      child: new Container(
         color: Color.fromRGBO(166, 165, 245, 1.0),
         margin: EdgeInsets.fromLTRB(10, 15, 10, 15),
         height:(100),
@@ -37,16 +41,16 @@ class Home extends StatelessWidget{
           children: <Widget>[
             //文字左对齐显示
             Text(
-                '测试数据title',
-                softWrap: true,//是否自动换行，若为false，文字将不考虑容器大小，单行显示，超出屏幕部分将默认截断处理
-                textAlign: TextAlign.left,//左对齐
-                overflow: TextOverflow.ellipsis,//超出部分省略号
-                maxLines: 1,
-                style:TextStyle(
-                  fontSize: 16,
-                  color:const Color(0xFF000000),
-                ),
+              '测试数据title',
+              softWrap: true,//是否自动换行，若为false，文字将不考虑容器大小，单行显示，超出屏幕部分将默认截断处理
+              textAlign: TextAlign.left,//左对齐
+              overflow: TextOverflow.ellipsis,//超出部分省略号
+              maxLines: 1,
+              style:TextStyle(
+                fontSize: 16,
+                color:const Color(0xFF000000),
               ),
+            ),
             Text(
               '测试数据 context说的发艾弗森的范德萨范德萨发大水发大水fdsafasdfadsfds',
               softWrap: true,
@@ -54,16 +58,20 @@ class Home extends StatelessWidget{
               overflow: TextOverflow.ellipsis,//超出部分省略号
               maxLines: 2,
               style:TextStyle(
-                fontSize: 14,
-                letterSpacing:2,//字母间距
-                color:const Color(0xFF000000),
-                height: (5+14)/14.0//单行高度=行高系数x字体高度
+                  fontSize: 14,
+                  letterSpacing:2,//字母间距
+                  color:const Color(0xFF000000),
+                  height: (5+14)/14.0//单行高度=行高系数x字体高度
               ),
             )
           ],
         ),
 
-      );
+      )
+    );
+
+//    return Text(posts[index].title);
+
   }
 
   @override
